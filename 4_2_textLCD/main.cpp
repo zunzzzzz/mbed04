a1950196236bcbfe87cea37056d281849aba63a2
zunzzz <a0983208130@gmail.com>
2020-03-22 14:59:34 +0800

    30 secs count down

--- 4_2_textLCD/main.cpp
+++ 4_2_textLCD/main.cpp
@@ -6,13 +6,16 @@ TextLCD lcd(D2, D3, D4, D5, D6, D7);
 int main()
 {
       int x=0;
-      lcd.printf("Hello World!\n");
+      lcd.printf("106060024\n");
       while(true)
       {
             led = !led;             // toggle led
             lcd.locate(5,1);
             lcd.printf("%5i",x);    //conuter display
             wait(1);
-            x++;
+            if(x == 30) {
+                  x = 1;
+            }
+            else x++;
       }
 }
